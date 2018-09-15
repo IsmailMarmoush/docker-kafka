ef18ee024c2d2939e81ff5184efccb7a24f77364
Ismail Marmoush <marmoushismail@gmail.com>
2018-09-15 23:24:30 +0200

    A Quick Commit!

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM openjdk:8-jre
 
 ARG KAFKA=kafka_2.11-2.0.0
 
-ENV KAFKA_HOME=/$KAFKA
+ENV KAFKA_HOME=/kafka
 ENV PATH=${PATH}:${KAFKA_HOME}/bin
 
 RUN wget -q http://www-eu.apache.org/dist/kafka/2.0.0/$KAFKA.tgz
 RUN tar xfz $KAFKA.tgz
+RUN mv $KAFKA $KAFKA_HOME
 RUN rm /$KAFKA.tgz
--- main.sh
+++ main.sh
@@ -3,16 +3,13 @@ set -e
 
 network=docker_network
 kafka_port=9092
+kafka_image=ismailmarmoush/kafka
+
 zookeeper_port=2181
+zookeeper_image=zookeeper
 
 build(){
-    docker build -t ismailmarmoush/kafka .
-}
-
-run(){
-    docker run -it \
-        --network="$NETWORK" \
-        ismailmarmoush/kafka
+    docker build -t $kafka_image .
 }
 
 bash(){
@@ -25,6 +22,30 @@ zookeeper(){
         zookeeper
 }
 
+kafka(){
+    hosts=$(get_hosts $zookeeper_image $zookeeper_port)
+    id=$(( ( RANDOM % 10 )  + 1 ))
+    echo "id: $id"
+    docker run -it \
+        --network="$NETWORK" \
+        $kafka_image \
+        /kafka/bin/kafka-server-start.sh \
+           /kafka/config/server.properties \
+           --override zookeeper.connect=$hosts \
+           --override broker.id=$id
+}
+
+kafka_producer(){
+    hosts=$(get_hosts $kafka_image $kafka_port)
+    docker exec -it $name bash -c "/kafka/bin/kafka-console-producer.sh --broker-list $hosts --topic test"
+}
+
+kafka_consumer(){
+    hosts=$(get_hosts $zookeeper_image $zookeeper_port)
+    docker exec -it $name bash -c "/kafka/bin/kafka-console-consumer.sh --bootstrap-server $hosts --topic test --from-beginning"
+}
+
+
 ####################################################################################################
 # Utils
 ####################################################################################################
@@ -33,14 +54,23 @@ docker_network(){
     docker network create -d bridge $network
 }
 
-get_ip(){
-    container=$1
-    docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container
+get_hosts(){
+    image_ancestor=$1
+    port=$2
+    hosts=$(get_container_name $image_ancestor | get_container_ip | sed "s/$/:$port,/")
+    hosts=$(echo $hosts | sed -e "s/\s//" -e "s/,$//")
+    echo $hosts
 }
 
-get_name(){
-    ancestor=$1
-    docker ps --filter "ancestor=$ancestor"  --format "{{.Names}}"
+get_container_ip(){
+    while read input; do
+        docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $input
+    done
+}
+
+get_container_name(){
+    input=$1
+    docker ps --filter "ancestor=$input"  --format "{{.Names}}"
 }
 
 $@
